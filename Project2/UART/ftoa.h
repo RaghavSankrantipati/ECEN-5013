79448432fac4e458786f6a5b7389451d16199e9f
Mounika1494 <edulamounika08@gmail.com>
2016-10-12 22:05:15 -0600

    header files

---
+++ Project2/UART/ftoa.h
@@ -0,0 +1,13 @@
+/*
+ * ftoa.h
+ *
+ *  Created on: Oct 12, 2016
+ *      Author: raghav
+ */
+
+#ifndef SOURCES_FTOA_H_
+#define SOURCES_FTOA_H_
+void ftoa(float n, char *res, int afterpoint);
+int intToStr(int x, char str[], int d);
+void reverse(char *str, int len);
+#endif /* SOURCES_FTOA_H_ */
