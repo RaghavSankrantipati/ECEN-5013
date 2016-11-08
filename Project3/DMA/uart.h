c83c85c532ab230ed251229c4a7997a7be63454a
Mounika1494 <edulamounika08@gmail.com>
2016-11-08 12:33:09 -0700

    dma

---
+++ Project3/DMA/uart.h
@@ -0,0 +1,15 @@
+/*
+ * uart.h
+ *
+ *  Created on: Oct 12, 2016
+ *      Author: raghav
+ */
+
+#ifndef SOURCES_UART_H_
+#define SOURCES_UART_H_
+
+
+
+
+void uart0_init (int sysclk, int baud);
+#endif /* SOURCES_UART_H_ */
