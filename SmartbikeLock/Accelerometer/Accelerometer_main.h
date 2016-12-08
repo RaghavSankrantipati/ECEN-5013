1aede357bd5490e844663ab774621a3dd900783b
Mounika1494 <edulamounika08@gmail.com>
2016-12-08 13:50:00 -0700

    updated comments

--- SmartbikeLock/Accelerometer/Accelerometer_main.h
+++ SmartbikeLock/Accelerometer/Accelerometer_main.h
@@ -14,7 +14,8 @@ uint8_t f;
 uint8_t CTRL_REG1_Data;
 uint8_t Int_SourceSystem;
 uint8_t Int_SourceTrans;
+/*** initialising Accelerometer**/
 void Accelerometer_main();
-
+/** Function Pointer for shifting between Accelerometer and Pressure Sesnor No arguments and return types**/
 void (*foo)();
 #endif /* SOURCES_ACCELEROMETER_MAIN_H_ */
