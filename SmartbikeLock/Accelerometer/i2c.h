3b05be6d1cafecb52000da54ec03de8166a2d3d9
Mounika1494 <edulamounika08@gmail.com>
2016-12-08 13:26:50 -0700

    updated comments

--- SmartbikeLock/Accelerometer/i2c.h
+++ SmartbikeLock/Accelerometer/i2c.h
@@ -11,22 +11,31 @@
 
 #define WRITE                   0x00  /* Master write  */
 #define READ                    0x01  /* Master read */
-//#define I2C_NACK()             I2C0_C1 |= I2C_C1_TXAK_MASK/*for sending NACK*/
-//#define I2C_ACK()              I2C0_C1 &= ~I2C_C1_TXAK_MASK/*for sending ACK*/
-//#define I2C_RS()               I2C0_C1 |= I2C_C1_RSTA_MASK/* for repeated start*/
-//#define I2C_Rx()               I2C0_C1 &= ~I2C_C1_TX_MASK/*I2C in RX mode*/
+/*Initialising I2C.Activating Clocks and I2C GPIO*/
 void Init_I2C(I2C_Type* i2c);
+/*Setup done for transmitting for selcting the slave and selection of mode*/
 void I2C_StartTx (char SlaveID, char Mode,I2C_Type* i2c);
+/*For writing to a regiter***/
 void I2CWriteRegister(char SlaveID, char RegisterAddress, char Data,I2C_Type* i2c);
+/*For reading from a regiter***/
 char I2CReadRegister(char SlaveID, char RegisterAddress,I2C_Type* i2c);
+/*for reading from multiple registers*/
 void I2CReadMultiRegisters(char SlaveID, char RegisterAddress, char n, char * r,I2C_Type* i2c);
+/*for writing*/
 void I2C_WRITE(I2C_Type* i2c,uint8_t data)__attribute__((always_inline));
+/*to send a start bit*/
 void I2C_Start(I2C_Type* i2c)__attribute__((always_inline));
+/* to send a stop bit*/
 void I2C_Stop(I2C_Type* i2c)__attribute__((always_inline));
+/* wait till ACk is recieved**/
 void I2C_Wait(I2C_Type* i2c)__attribute__((always_inline));
+/*send an NACK if it is the last byte to be read*/
 void I2C_NACK(I2C_Type* i2c)__attribute__((always_inline));
+/*send an ACK for reading more bytes of data**/
 void I2C_ACK(I2C_Type* i2c)__attribute__((always_inline));
+/*Send restart**/
 void I2C_RS(I2C_Type* i2c)__attribute__((always_inline));
+/*for setting mode for reading using i2c**/
 void I2C_Rx(I2C_Type* i2c)__attribute__((always_inline));
 
 #endif /* SOURCES_I2C_H_ */
