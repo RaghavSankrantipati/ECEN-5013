566c59ffd7f78cc38658043740c1067b50960357
Mounika1494 <edulamounika08@gmail.com>
2016-12-08 13:14:05 -0700

    Created header file for Accelerometer registers

---
+++ SmartbikeLock/Accelerometer/MMA8451q.h
@@ -0,0 +1,15 @@
+
+#define Acceler_address 0x1D
+#define CTRL1_REG_ADDR  0x2A
+#define CONFIG_REG_ADDR 0x1D
+#define THRS_REG_ADDR   0x1F
+#define DBNS_REG_ADDR   0x20
+#define CTRL_REG4_ADDR  0x2D
+#define CTRL_REG5_ADDR  0x2E
+#define CTRL1_REG_VAL  0x18
+#define CONFIG_REG_VAL 0x16
+#define THRS_REG_VAL   0x08
+#define DBNS_REG_VAL   0x05
+#define CTRL_REG4_VAL  0x20
+#define CTRL_REG5_VAL  0x20
+#define ACC_ACTIVEMODE 0x01
