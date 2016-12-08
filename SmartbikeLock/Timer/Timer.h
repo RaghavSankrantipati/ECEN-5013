392efe9f45890bed94de712fddfa6d14b294443f
raghav6092 <raghavendra.sankrantipati@gmail.com>
2016-12-08 04:46:00 -0700

    Add files via upload

---
+++ SmartbikeLock/Timer/Timer.h
@@ -0,0 +1,15 @@
+/*
+ * Timer.h
+ *
+ *  Created on: Dec 7, 2016
+ *      Author: sasa6092
+ */
+
+#ifndef SOURCES_TIMER_H_
+#define SOURCES_TIMER_H_
+
+
+
+void TPM2_Setup(void);
+
+#endif /* SOURCES_TIMER_H_ */
